4f605c6b0df0289456a254b7815fa01556f3788a
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-10-24 12:28:03 +0900

    #81 textField Left Padding Extension

---
+++ MateList/ViewController/NickName/NickNameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NickNameViewController.swift
+//  MateList
+//
+//  Created by 강민규 on 2022/10/24.
+//
+
+import UIKit
+
+class NickNameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
