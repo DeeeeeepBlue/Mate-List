7fae25871e2c1e49562b66eb41ce909c4368813a
leedoyeon849 <do83430208@gmail.com>
2022-12-13 14:36:04 +0900

    [Refac] 코디네이터 패턴 적용 중

---
+++ MateList/Presentation/Common/Protocol/AppCoordinator.swift
@@ -0,0 +1,13 @@
+//
+//  AppCoordinator.swift
+//  MateList
+//
+//  Created by DOYEON LEE on 2022/12/13.
+//
+
+import Foundation
+
+protocol AppCoordinator: Coordinator {
+    func showLoginFlow()
+    func showTabBarFlow()
+}
