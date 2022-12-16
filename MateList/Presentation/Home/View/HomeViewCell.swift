2947e5e6ce574c57b7513e507d130e354dbdcde5
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-12-17 03:39:38 +0900

    TableView Cell View 적합도 넣기

--- MateList/Presentation/Home/View/HomeViewCell.swift
+++ MateList/Presentation/Home/View/HomeViewCell.swift
@@ -11,26 +11,21 @@ import RxSwift
 import SnapKit
 
 class HomeViewCell: UITableViewCell {
+    // MARK: - Properties
     static var cellIdentifier = "thisIsHome"
     
-    private(set) lazy var bottomContainer: UIStackView = {
-        let view = UIStackView()
-
-        return view
-    }()
-    
-    private(set) lazy var spacer: UIView = {
-        let view = UIView()
-        view.setContentHuggingPriority(.defaultLow, for: .horizontal)
-        return view
-    }()
+    private(set) lazy var topConatiner: UIStackView = UIStackView()
+    private(set) lazy var bottomContainer: UIStackView = UIStackView()
     
     private(set) lazy var matchLabel = self.createLabel(size: 14, family: .bold)
+    private(set) lazy var matchPercentView = self.createUIView(width: 70, height: 20)
+    private(set) lazy var matchPercentLabel = self.createLabel(size: 14, family: .medium)
     private(set) lazy var titleLabel = self.createLabel(size: 14)
     private(set) lazy var contentLabel = self.createLabel(size: 14)
     private(set) lazy var dateLabel = self.createLabel(size: 14)
     private(set) lazy var userLabel = self.createLabel(size: 14)
     
+    // MARK: - Init
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         self.configureUI()
@@ -40,39 +35,62 @@ class HomeViewCell: UITableViewCell {
         super.init(coder: aDecoder)
         self.configureUI()
     }
-    
+}
+
+//MARK: - ConfigureUI
+extension HomeViewCell {
     func updateUI(post: Post) {
         self.matchLabel.text = "나와의 적합도"
+        self.matchPercentLabel.text = "100%"
         self.titleLabel.text = post.title
         self.contentLabel.text = post.contents
         self.dateLabel.text = post.date
         self.userLabel.text = post.author
     }
-}
-
-private extension HomeViewCell {
-    func configureUI() {
+    
+    private func configureUI() {
         self.contentView.snp.makeConstraints { make in
             make.top.leading.trailing.equalToSuperview()
             make.height.equalTo(120)
         }
         
         // AddView
-        self.contentView.addSubview(matchLabel)
+        self.topConatiner.addArrangedSubview(matchLabel)
+        self.topConatiner.addArrangedSubview(matchPercentView)
+        self.topConatiner.addArrangedSubview(createSpacer())
+        self.matchPercentView.addSubview(matchPercentLabel)
+        self.contentView.addSubview(topConatiner)
         self.contentView.addSubview(titleLabel)
         self.contentView.addSubview(contentLabel)
         self.contentView.addSubview(bottomContainer)
         self.bottomContainer.addArrangedSubview(dateLabel)
-        self.bottomContainer.addArrangedSubview(spacer)
+        self.bottomContainer.addArrangedSubview(createSpacer())
         self.bottomContainer.addArrangedSubview(userLabel)
         
         // Constraints
-        self.matchLabel.snp.makeConstraints { make in
+        self.topConatiner.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(16)
             make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-16)
+            make.height.equalTo(20)
+            
+            self.matchLabel.snp.makeConstraints { make in
+                make.top.equalToSuperview()
+                make.leading.equalToSuperview()
+            }
+            self.matchPercentView.snp.makeConstraints { make in
+                make.top.equalToSuperview()
+                make.leading.equalTo(matchLabel.snp.trailing)
+            }
         }
+        
+        self.matchPercentLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.leading.equalToSuperview().offset(4)
+        }
+       
         self.titleLabel.snp.makeConstraints { make in
-            make.top.equalTo(matchLabel.snp.bottom).offset(4)
+            make.top.equalTo(topConatiner.snp.bottom).offset(4)
             make.leading.equalToSuperview().offset(16)
         }
         self.contentLabel.snp.makeConstraints { make in
@@ -84,17 +102,22 @@ private extension HomeViewCell {
             make.leading.equalToSuperview().offset(16)
             make.trailing.equalToSuperview().offset(-16)
             make.bottom.equalToSuperview().offset(-16)
+            
+            self.dateLabel.snp.makeConstraints { make in
+                make.top.equalToSuperview()
+                make.leading.equalToSuperview()
+            }
+            self.userLabel.snp.makeConstraints { make in
+                make.top.equalToSuperview()
+                make.trailing.equalToSuperview()
+            }
         }
-        self.dateLabel.snp.makeConstraints { make in
-            make.top.equalToSuperview()
-            make.leading.equalToSuperview()
-        }
-        self.userLabel.snp.makeConstraints { make in
-            make.top.equalToSuperview()
-            make.trailing.equalToSuperview()
-        }
+        
     }
-    
+}
+
+//MARK: - Create func
+private extension HomeViewCell {
     func createLabel(size: CGFloat, family: UIFont.Family = .regular) -> UILabel {
         let label = UILabel()
         label.font = .notoSans(size: size, family: family)
@@ -107,7 +130,12 @@ private extension HomeViewCell {
             make.width.equalTo(width)
             make.height.equalTo(height)
         }
-        view.layer.borderWidth = 0.5
+        return view
+    }
+    
+    func createSpacer() -> UIView {
+        let view = UIView()
+        view.setContentHuggingPriority(.defaultLow, for: .horizontal)
         return view
     }
 }
