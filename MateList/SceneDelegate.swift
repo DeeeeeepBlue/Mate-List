36c07f831415fb68277d7cb79a06ea4043eac97a
leedoyeon849 <do83430208@gmail.com>
2022-12-18 13:49:39 +0900

    [Refac] AppCoordinator Protocol 및 구현체 세팅

--- MateList/SceneDelegate.swift
+++ MateList/SceneDelegate.swift
@@ -17,6 +17,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        self.appCoordinator = DefaultAppCoordinator(navigationController)
+        self.appCoordinator?.start()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
---
+++ MateList/Util/CoordinatorType.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatorType.swift
+//  MateList
+//
+//  Created by DOYEON LEE on 2022/12/18.
+//
+
+import Foundation
+
+enum CoordinatorType {
+//    case app, login, tab
+//    case home, record, mate, mypage
+//    case setting, running
+//    case signUp, addMate
+    case home, detail, scrap, info, survey, write
+    case tab
+}
