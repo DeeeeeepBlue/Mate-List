0758a820a7ed9ed5daecb7370597aa7e680b8f73
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-12-19 14:29:59 +0900

    Domain Entity 수정

---
+++ MateList/Domain/Entity/Response/Comment.swift
@@ -0,0 +1,16 @@
+//
+//  Comment.swift
+//  checkMate
+//
+//  Created by 강민규 on 2022/02/18.
+//
+
+import Foundation
+
+public struct Reply {
+    let author: String
+    let contents: String
+    let date : String
+    let uid : String
+    let docid : String
+}
---
+++ MateList/Domain/Entity/Response/Post.swift
@@ -0,0 +1,19 @@
+//
+//  Post.swift
+//  checkMate
+//
+//  Created by 강민규 on 2022/02/18.
+//
+
+import Foundation
+
+
+public struct Post {
+    var uid : String
+    let author: String
+    let title: String
+    let contents: String
+    var isScrap : Bool
+    let date : String
+    let pid : String
+}
