badb79458b462cd09901bb9833a99ced0866636d
leedoyeon849 <do83430208@gmail.com>
2022-12-24 01:25:45 +0900

    [Feat] DefaultAppCoordinator 수정
     - finishDelegate 추가
    - showMainFlow() 구현

--- MateList/Presentation/Common/Coordinator/DefaultAppCoordinator.swift
+++ MateList/Presentation/Common/Coordinator/DefaultAppCoordinator.swift
@@ -10,21 +10,35 @@ import UIKit
 
 class DefaultAppCoordinator: AppCoordinator {
 
+    weak var finishDelegate: CoordinatorFinishDelegate?
     var navigationController: UINavigationController
     var childCoordinators: [Coordinator]
     var type: CoordinatorType
     
-    func start() {
-        <#code#>
-    }
-    
-    func showMainFlow() {
-        <#code#>
-    }
-    
     required init(_ navigationController: UINavigationController) {
         self.navigationController = navigationController
+        self.childCoordinators = []
         self.type = CoordinatorType.home
     }
     
+    func start() {
+        showMainFlow()
+    }
+    
+    func showMainFlow() {
+        let homeCoordinator = DefaultHomeCoordinator(self.navigationController)
+        homeCoordinator.finishDelegate = self
+        homeCoordinator.start()
+        childCoordinators.append(homeCoordinator)
+    }
+    
+}
+
+extension DefaultAppCoordinator: CoordinatorFinishDelegate {
+    func coordinatorDidFinish(childCoordinator: Coordinator) {
+        // 자식 뷰를 삭제하는 델리게이트 (자식 -> 부모 접근 -> 부모에서 자식 삭제)
+        self.childCoordinators = self.childCoordinators
+            .filter({ $0.type != childCoordinator.type })
+        childCoordinator.navigationController.popToRootViewController(animated: true)
+    }
 }
