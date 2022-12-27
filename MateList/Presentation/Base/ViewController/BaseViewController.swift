6681c23050209c4b1ddf482066a1244f71bae153
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-12-28 03:13:02 +0900

    [Feat] init 에러 제거

--- MateList/Presentation/Base/ViewController/BaseViewController.swift
+++ MateList/Presentation/Base/ViewController/BaseViewController.swift
@@ -14,7 +14,7 @@ class BaseViewController: UIViewController {
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
     }
     
     override func viewDidLoad() {
