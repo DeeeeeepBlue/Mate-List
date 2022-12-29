631ecd22706709959947e6d8a20761278066e24e
leedoyeon849 <do83430208@gmail.com>
2022-12-29 22:03:50 +0900

    [Refac] Info, Scrap 폴더링 정리

---
+++ MateList/Presentation/Info/Coordinator/DefaultInfoCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  DefaultInfoCoordinator.swift
+//  MateList
+//
+//  Created by DOYEON LEE on 2022/12/29.
+//
+
+import Foundation
---
+++ MateList/Presentation/Scrap/Coordinator/DefaultScrapCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  DefaultScrapCoordinator.swift
+//  MateList
+//
+//  Created by DOYEON LEE on 2022/12/29.
+//
+
+import Foundation
