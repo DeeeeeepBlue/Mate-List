e3cfdf319c4119dee92da5cfe0d633b037c40fa9
leedoyeon849 <do83430208@gmail.com>
2022-12-29 22:09:02 +0900

    [Feat] Info, Scrap Coordinator 틀 생성

--- MateList/Presentation/Info/Coordinator/DefaultInfoCoordinator.swift
+++ MateList/Presentation/Info/Coordinator/DefaultInfoCoordinator.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import UIKit
+
+class DefaultInfoCoordinator{
+    
+    weak var finishDelegate: CoordinatorFinishDelegate?
+    var navigationController: UINavigationController
+    var infoViewController: Info // TODO: Info->InfoViewController로 클래스명 변경 필요
+    var childCoordinators: [Coordinator] = []
+    var type: CoordinatorType = .home
+    
+    required init(_ navigationController: UINavigationController) {
+        self.navigationController = navigationController
+        self.infoViewController = Info()
+    }
+    
+    func start() {
+        self.navigationController.pushViewController(self.infoViewController, animated: true)
+    }
+}
+
+extension DefaultInfoCoordinator: CoordinatorFinishDelegate {
+    func coordinatorDidFinish(childCoordinator: Coordinator) {
+        // 자식 뷰를 삭제하는 델리게이트 (자식 -> 부모 접근 -> 부모에서 자식 삭제)
+        self.childCoordinators = self.childCoordinators
+            .filter({ $0.type != childCoordinator.type })
+        childCoordinator.navigationController.popToRootViewController(animated: true)
+    }
+}
--- MateList/Presentation/Scrap/Coordinator/DefaultScrapCoordinator.swift
+++ MateList/Presentation/Scrap/Coordinator/DefaultScrapCoordinator.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import UIKit
+
+class DefaultScrapCoordinator{
+    
+    weak var finishDelegate: CoordinatorFinishDelegate?
+    var navigationController: UINavigationController
+    var scrapViewController: ScrapViewController
+    var childCoordinators: [Coordinator] = []
+    var type: CoordinatorType = .home
+    
+    required init(_ navigationController: UINavigationController) {
+        self.navigationController = navigationController
+        self.scrapViewController = ScrapViewController()
+    }
+    
+    func start() {
+        self.navigationController.pushViewController(self.scrapViewController, animated: true)
+    }
+}
+
+extension DefaultScrapCoordinator: CoordinatorFinishDelegate {
+    func coordinatorDidFinish(childCoordinator: Coordinator) {
+        // 자식 뷰를 삭제하는 델리게이트 (자식 -> 부모 접근 -> 부모에서 자식 삭제)
+        self.childCoordinators = self.childCoordinators
+            .filter({ $0.type != childCoordinator.type })
+        childCoordinator.navigationController.popToRootViewController(animated: true)
+    }
+}
