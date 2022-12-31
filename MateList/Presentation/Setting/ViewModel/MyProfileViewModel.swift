62e70bc591b8a0020489360edba2c7313f1a283c
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2023-01-01 00:04:25 +0900

    [Feat] MyProfileViewModel 기초 작업

--- MateList/Presentation/Setting/ViewModel/MyProfileViewModel.swift
+++ MateList/Presentation/Setting/ViewModel/MyProfileViewModel.swift
@@ -5,4 +5,33 @@
 //  Created by 강민규 on 2022/12/31.
 //
 
-import Foundation
+import UIKit
+
+import RxSwift
+
+protocol MyProfileViewModelProtocol {
+    var fetchUser: AnyObserver<User> { get }
+    
+    var name: Observable<String> { get }
+    var email: Observable<String> { get }
+}
+
+class MyProfileViewModel: MyProfileViewModelProtocol {
+    var fetchUser: AnyObserver<User> 
+    
+    var name: Observable<String>
+    var email: Observable<String>
+    
+    init() {
+        let fetching = BehaviorSubject<User>(value: Dummy.mingyu)
+        
+        fetchUser = fetching.asObserver()
+        
+        name = fetching
+            .map { $0.name }
+        
+        
+        email = fetching
+            .map { $0.email }
+    }
+}
