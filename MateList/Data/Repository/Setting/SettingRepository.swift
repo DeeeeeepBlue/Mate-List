9f0d63c11c8102a0661eb91481185dd14f746012
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2023-01-02 01:47:17 +0900

    [Feat] Settiing의 Repo랑 Usecase 만들기 초기화

---
+++ MateList/Data/Repository/Setting/SettingRepository.swift
@@ -0,0 +1,8 @@
+//
+//  SettingRepository.swift
+//  MateList
+//
+//  Created by 강민규 on 2023/01/02.
+//
+
+import Foundation
---
+++ MateList/Domain/UseCase/Setting/MyProfileUseCase.swift
@@ -0,0 +1,8 @@
+//
+//  MyProfileUseCase.swift
+//  MateList
+//
+//  Created by 강민규 on 2023/01/02.
+//
+
+import Foundation
