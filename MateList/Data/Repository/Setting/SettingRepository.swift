f82032c3eca5ea4756935a9c96376ea1bb79f6c4
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2023-01-02 03:47:02 +0900

    [Feat] 애플 로그인 로직 추가

--- MateList/Data/Repository/Setting/SettingRepository.swift
+++ MateList/Data/Repository/Setting/SettingRepository.swift
@@ -5,4 +5,25 @@
 //  Created by 강민규 on 2023/01/02.
 //
 
-import Foundation
+import FirebaseAuth
+
+class SettingRepository: SettingRepositoryProtocol {
+    func googleCredential() -> AuthCredential {
+        
+    }
+    /* TODO: SettingVC에 있는 Apple SignIn 로직 리팩토링 고려
+    func appleCredential() -> AuthCredential {
+        
+    }
+     */
+    
+    func authSignIn(credential: AuthCredential) {
+        
+    }
+    
+    func registUser(user: User) {
+        
+    }
+    
+    
+}
--- MateList/Presentation/Setting/ViewController/SettingViewController.swift
+++ MateList/Presentation/Setting/ViewController/SettingViewController.swift
@@ -6,12 +6,17 @@
 //
 
 import UIKit
+import AuthenticationServices
+import CryptoKit
+import FirebaseAuth
 
 import SnapKit
 import RxSwift
 
 class SettingViewController: BaseViewController {
     //MARK: - Properties
+    private let repository = SettingRepository()
+    
     private let myProfileView = MyProfileView()
     private let surveyButton = SurveyButton()
     private let questionButton = QuestionButton()
@@ -21,7 +26,8 @@ class SettingViewController: BaseViewController {
     
     private let nickNameView = NickNameView()
     
-    
+    fileprivate var currentNonce: String?
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -96,4 +102,115 @@ class SettingViewController: BaseViewController {
         
     }
 
+}
+
+//MARK: - Apple SignIn
+
+extension SettingViewController: ASAuthorizationControllerPresentationContextProviding {
+    /// - Tag: provide_presentation_anchor
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
+}
+
+
+extension SettingViewController: ASAuthorizationControllerDelegate {
+    /// - Tag: did_complete_authorization
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            guard let nonce = currentNonce else {
+                fatalError("Invalid state: A login callback was received, but no login request was sent.")
+            }
+            guard let appleIDToken = appleIDCredential.identityToken else {
+                print("Unable to fetch identity token")
+                return
+            }
+            guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+                print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+                return
+            }
+            // Initialize a Firebase credential.
+            let credential = OAuthProvider.credential(withProviderID: "apple.com",
+                                                      idToken: idTokenString,
+                                                      rawNonce: nonce)
+            // Sign in with Firebase.
+            repository.authSignIn(credential: credential)
+            
+            // User is signed in to Firebase with Apple.
+            // ...
+        }
+    }
+    
+    
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        // Handle error.
+        print("Sign in with Apple errored: \(error)")
+    }
+    
+    
+    /// 🔥 firebase hasing function
+    /// Adapted from https://auth0.com/docs/api-auth/tutorials/nonce#generate-a-cryptographically-random-nonce
+    func randomNonceString(length: Int = 32) -> String {
+        precondition(length > 0)
+        let charset: [Character] =
+        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        var result = ""
+        var remainingLength = length
+        
+        while remainingLength > 0 {
+            let randoms: [UInt8] = (0 ..< 16).map { _ in
+                var random: UInt8 = 0
+                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+                if errorCode != errSecSuccess {
+                    fatalError(
+                        "Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)"
+                    )
+                }
+                return random
+            }
+            
+            randoms.forEach { random in
+                if remainingLength == 0 {
+                    return
+                }
+                
+                if random < charset.count {
+                    result.append(charset[Int(random)])
+                    remainingLength -= 1
+                }
+            }
+        }
+        
+        return result
+        
+        
+    }
+    
+    @available(iOS 13, *)
+    func startSignInWithAppleFlow() {
+        let nonce = randomNonceString()
+        currentNonce = nonce
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        request.nonce = sha256(nonce)
+        
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
+    
+    @available(iOS 13, *)
+    func sha256(_ input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA256.hash(data: inputData)
+        let hashString = hashedData.compactMap {
+            String(format: "%02x", $0)
+        }.joined()
+        
+        return hashString
+        
+    }
 }
