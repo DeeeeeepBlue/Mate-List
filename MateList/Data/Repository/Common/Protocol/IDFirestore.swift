b0d64c0e7467939d0395418a4bef6fe3dba3173f
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2023-01-04 05:10:30 +0900

    [Feat] Delete, Check 기능 Repo에 추가

--- MateList/Data/Repository/Common/Protocol/IDFirestore.swift
+++ MateList/Data/Repository/Common/Protocol/IDFirestore.swift
@@ -11,4 +11,13 @@ protocol IDFirestore {
     static func userName(uid: String) -> Observable<String>
     static func habitCheck(uid: String) -> Observable<HabitCheck>
     static func myUID() -> Observable<String>
+    
+    static func isExistUser(uid: String) -> Observable<Bool>
+    static func isBlackUser(uid: String) -> Observable<Bool>
+    
+    static func deleteAllPost(uid: String)
+    static func deleteAllScrap(uid: String)
+    static func deleteAllHabit(uid: String)
+    static func deleteUser(uid: String)
+    
 }
