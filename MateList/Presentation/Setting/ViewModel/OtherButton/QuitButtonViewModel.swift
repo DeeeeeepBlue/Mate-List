1dde92df79af76f64e64879243c5cedd5246cd1f
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2023-01-04 03:38:25 +0900

    [Feat] Button ViewModel 추가

---
+++ MateList/Presentation/Setting/ViewModel/OtherButton/QuitButtonViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  QuitButtonViewModel.swift
+//  MateList
+//
+//  Created by 강민규 on 2023/01/04.
+//
+
+import Foundation
---
+++ MateList/Presentation/Setting/ViewModel/OtherButton/SignOutButtonViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SignOutButtonViewModel.swift
+//  MateList
+//
+//  Created by 강민규 on 2023/01/04.
+//
+
+import Foundation
