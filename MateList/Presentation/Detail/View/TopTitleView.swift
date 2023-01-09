dbcfd6f66997669e163a6c4958884893b1517d0b
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2023-01-10 00:51:03 +0900

    [Feat] 제목, 작성자 뷰 추가

--- MateList/Presentation/Detail/View/TopTitleView.swift
+++ MateList/Presentation/Detail/View/TopTitleView.swift
@@ -10,15 +10,38 @@ import UIKit
 import SnapKit
 
 class TopTitleView: BaseView {
-    
+    private(set) lazy var userLabel = self.createLabel(size: 14)
+    private(set) lazy var titleLabel = self.createLabel(size: 18,family: .bold)
     private let reportButton = ReportButton()
+    private let deleteButton = DeleteButton()
     
     override func configureUI() {
+        self.addSubview(userLabel)
+        self.addSubview(titleLabel)
         self.addSubview(reportButton)
+        self.addSubview(deleteButton)
+        
+        userLabel.text = "고먐미의 글"
+        titleLabel.text = "제목"
+        
+        userLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(12)
+            make.top.equalToSuperview().inset(12)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(12)
+            make.top.equalTo(userLabel.snp.bottom).offset(12)
+        }
         
         reportButton.snp.makeConstraints { make in
             make.trailing.equalToSuperview().inset(12)
             make.top.equalToSuperview().inset(12)
+        }
+        
+        deleteButton.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().inset(12)
+            make.top.equalTo(reportButton.snp.bottom).offset(12)
         }
     }
 }
